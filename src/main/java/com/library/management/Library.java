e727e9488f95a4909019e44f9298d187976e6e06
raj-dave-2004 <daveraj2004@gmail.com>
2024-09-01 18:10:46 +0530

    Implement book borrowing feauture

--- src/main/java/com/library/management/Library.java
+++ src/main/java/com/library/management/Library.java
@@ -19,4 +19,24 @@ public class Library {
     public List<Book> getAllBooks() {
         return books;
     }
+
+    //Implementation of borrowing books feauture
+
+    public Book getBookByISBN(String isbn) {
+        for (Book book : books) {
+            if (book.getIsbn().equals(isbn)) {
+                return book;
+            }
+        }
+        return null; // Return null if no book is found
+    }
+
+    public void borrowBook(String isbn) throws Exception {
+        Book book = getBookByISBN(isbn);
+        if (book != null && book.isAvailable()) {
+            book.setAvailable(false);
+        } else {
+            throw new Exception("Book is not available or not exist.");
+        }
+    }
 }
